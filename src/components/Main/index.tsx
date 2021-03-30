e9c52f97fe027f36120ed3770f6afe38d57111f7
Leandro <leandro@Leandros-MacBook-Pro.local>
2021-03-30 18:33:59 -0300

    inserindo styled components

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+    <S.Warpper>
+        <S.Title>React Avançado</S.Title>
+        <S.Description>
+            Typescript, ReactJS, NexJS e Styled Components
+        </S.Description>
+    </S.Warpper>
+)
+
+export default Main
